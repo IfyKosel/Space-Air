fd6d6e5f3c5669cb2287007de8003918c4b5b104
Ifeoma <ifeoma.ofodile.231807@unn.edu.ng>
2024-05-29 22:15:12 +0100

    Add-on: safe

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -7,7 +7,7 @@ const f = createUploadthing();
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f(
-	{ image: { maxFileSize: "2MB" } }
+	{ image: { maxFileSize: "1MB" } }
 )    
     .onUploadComplete(
 		async ({ metadata, file }) => {
